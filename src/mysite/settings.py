1cf763990bd6d808d9f1d2486514725deb0eaa2a
Bernard <hollandsbernard@gmail.com>
2020-03-21 14:16:27 +0000

    Created templates

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     #my apps
     'personal',
 
-    
+
     #django apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
