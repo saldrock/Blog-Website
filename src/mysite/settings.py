59e47db7b636f69cba13eb859e252bc8466dec16
Bernard <hollandsbernard@gmail.com>
2020-03-21 14:11:35 +0000

    Created personal (mismatch) app

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -28,15 +28,22 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
+    #my apps
+    'personal',
+
+    
+    #django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
