0456c7f5b17ffaeb4701ed0e743ddd28b88196b5
Bernard <hollandsbernard@gmail.com>
2020-03-21 15:12:18 +0000

    added snippets and context

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-	print(request.headers)
-	return render(request, "base.html", {})
+	context = {}
+	list_of_values = []
+	list_of_values.append("first entry")
+	list_of_values.append("second entry")
+	list_of_values.append("third entry")
+	list_of_values.append("fourth entry")
+	context["list_of_values"] = list_of_values
+	return render(request, "personal/home.html", context)
